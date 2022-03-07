58e13fd7f7481be70f976e39e0da53bf5061895a
k0pernicus <dev@0xc0ff33.me>
2022-03-07 16:48:06 +0100

    Updated documentation for shader_utils.h

--- shader_utils.h
+++ shader_utils.h
@@ -16,17 +16,66 @@ namespace ShaderUtils
     {
 
     private:
+        /**
+         * @brief The vertex shader ID, as an optional
+         */
         std::optional<unsigned int> vertexShader = std::nullopt;
+
+        /**
+         * @brief The fragment shader ID, as an optional
+         */
         std::optional<unsigned int> fragmentShader = std::nullopt;
+
+        /**
+         * @brief The GPU program ID (or shader ID), as an optional
+         */
         std::optional<unsigned int> program = std::nullopt;
+
+        /**
+         * @brief Stores if the shader has been registered or not
+         */
         bool registered = false;
 
     public:
+        /**
+         * @brief Constructor
+         */
         Program();
+
+        /**
+         * @brief Destructor
+         */
         ~Program();
+
+        /**
+         * @brief Register a shader
+         *
+         * @param shader_type The type: fragment or vertex
+         * @param shader_source The source code as a string
+         * @return true The shader has been successfully registered
+         * @return false The shader has not been registered - error is logged
+         */
         bool registerShader(const Type shader_type, const char *shader_source);
+
+        /**
+         * @brief Register the GPU program (or shader), after compilation
+         * of the fragment and vertex shaders.
+         *
+         * @return true The shader has been successfully registered
+         * @return false The shader has not been successfully registered - error is logged
+         */
         bool registerProgram();
+
+        /**
+         * @brief Returns the GPU program ID object, as optional
+         *
+         * @return std::optional<unsigned int>
+         */
         std::optional<unsigned int> getProgram() const;
+
+        /**
+         * @brief Returns if the GPU program object has been registered or not
+         */
         bool programIsRegistered() const;
     };
 
